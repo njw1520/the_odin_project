5776936dc7787a27c96d35826e56b63caa9c638a
Nate Webb <145239754+njw1520@users.noreply.github.com>
2024-05-20 21:35:34 -0400

    Added playRound functionality to RPS

--- rock_paper_scissors/main.js
+++ rock_paper_scissors/main.js
@@ -2,13 +2,63 @@ function getComputerChoice() {
   let compChoice = Math.floor(Math.random() * 3);
   switch (compChoice) {
     case 0:
-      return "Rock";
+      return "rock";
       break;
     case 1:
-      return "Paper";
+      return "paper";
       break;
     case 2:
-      return "Scissors";
+      return "scissors";
       break;
+  }
+}
+
+function getUserChoice() {
+  let userChoice = prompt(
+    "Enter 'rock', 'paper', or 'scissors': ",
+  ).toLowerCase();
+
+  switch (userChoice) {
+    case "rock":
+    case "paper":
+    case "scissors":
+      return userChoice;
+      break;
+    default:
+      return "Invalid selection!";
+      break;
+  }
+}
+
+let userScore = 0;
+let computerScore = 0;
+
+function playRound() {
+  //Get user and computer choices for the round.
+  const userSelection = getUserChoice();
+  const computerSelection = getComputerChoice();
+
+  //Game logic
+  if (userSelection === "rock" && computerSelection === "scissors") {
+    console.log("You win! Rock beats Scissors");
+    userScore++;
+  } else if (userSelection === "paper" && computerSelection === "rock") {
+    console.log("You win! Paper beats Rock");
+    userScore++;
+  } else if (userSelection === "scissors" && computerSelection === "paper") {
+    console.log("You win! Scissors beat Paper");
+    userScore++;
+  } else if (userSelection === "rock" && computerSelection === "paper") {
+    console.log("You lose! Paper beats rock");
+    computerScore++;
+  } else if (userSelection === "scissors" && computerSelection === "rock") {
+    console.log("You lose! Rock beats scissors");
+    computerScore++;
+  } else if (userSelection === "paper" && computerSelection === "scissors") {
+    console.log("You lose! Scissors beats paper");
+    computerScore++;
+    console.log();
+  } else {
+    console.log("It's a draw!");
   }
 }
