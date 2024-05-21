2ad62dc5a497815bc3c1d7123cc7c4344bfa3cc6
Nate Webb <145239754+njw1520@users.noreply.github.com>
2024-05-20 21:09:52 -0400

    Created rock_paper_scissors repo and created computer choice function

---
+++ rock_paper_scissors/main.js
@@ -0,0 +1,14 @@
+function getComputerChoice() {
+  let compChoice = Math.floor(Math.random() * 3);
+  switch (compChoice) {
+    case 0:
+      return "Rock";
+      break;
+    case 1:
+      return "Paper";
+      break;
+    case 2:
+      return "Scissors";
+      break;
+  }
+}
