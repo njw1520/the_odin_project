88a0005b672ca3f81b92140e1e10ce3949f133a0
Nate Webb <145239754+njw1520@users.noreply.github.com>
2024-05-31 22:01:05 -0400

    Completed basic RPS UI functionality

--- rock_paper_scissors/main.js
+++ rock_paper_scissors/main.js
@@ -14,9 +14,7 @@ function getComputerChoice() {
 }
 
 function getUserChoice() {
-  let userChoice = prompt(
-    "Enter 'rock', 'paper', or 'scissors': ",
-  ).toLowerCase();
+  let userChoice = selection;
 
   switch (userChoice) {
     case "rock":
@@ -33,54 +31,64 @@ function getUserChoice() {
 let userScore = 0;
 let computerScore = 0;
 
-function playRound() {
+function playRound(userClick) {
   //Get user and computer choices for the round.
-  const userSelection = getUserChoice();
+  const userSelection = userClick;
   const computerSelection = getComputerChoice();
 
   //Game logic
   if (userSelection === "rock" && computerSelection === "scissors") {
-    console.log("You win! Rock beats Scissors");
     userScore++;
+    document.querySelector(".winner").innerText =
+      "You win! Rock beats Scissors";
   } else if (userSelection === "paper" && computerSelection === "rock") {
-    console.log("You win! Paper beats Rock");
     userScore++;
+    document.querySelector(".winner").innerText = "You win! Paper beats Rock";
   } else if (userSelection === "scissors" && computerSelection === "paper") {
-    console.log("You win! Scissors beat Paper");
     userScore++;
+    document.querySelector(".winner").innerText =
+      "You win! Scissors beat Paper";
   } else if (userSelection === "rock" && computerSelection === "paper") {
-    console.log("You lose! Paper beats rock");
     computerScore++;
+    document.querySelector(".winner").innerText = "You lose! Paper beats rock";
   } else if (userSelection === "scissors" && computerSelection === "rock") {
-    console.log("You lose! Rock beats scissors");
     computerScore++;
+    document.querySelector(".winner").innerText =
+      "You lose! Rock beats scissors";
   } else if (userSelection === "paper" && computerSelection === "scissors") {
-    console.log("You lose! Scissors beats paper");
     computerScore++;
-    console.log();
+    document.querySelector(".winner").innerText =
+      "You lose! Scissors beats paper";
   } else {
-    console.log("It's a draw!");
+    document.querySelector(".winner").innerText = "It's a draw!";
   }
+
+  document.querySelector("#player").innerText = `${userScore}`;
+  document.querySelector("#computer").innerText = `${computerScore}`;
 }
 
 function finalScore(userScore, computerScore) {
-  console.log("Final Scores:");
-  console.log(`User: ${userScore}`);
-  console.log(`Computer: ${computerScore}`);
   if (userScore > computerScore) {
-    console.log("User wins!");
+    document.querySelector(".winner").innerText = "Game Over: User wins!";
   } else if (computerScore > userScore) {
-    console.log("Computer wins!");
-  } else {
-    console.log("Its a draw!");
+    document.querySelector(".winner").innterText = "Game Over: Computer wins!";
   }
 }
 
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    console.log(`Current User Score: ${userScore}`);
-    console.log(`Current Computer Score: ${computerScore}`);
-    playRound();
+function playGame(clickChoice) {
+  playRound(clickChoice);
+  if (userScore === 5 || computerScore === 5) {
+    finalScore(userScore, computerScore);
+    computerScore = 0;
+    userScore = 0;
   }
-  finalScore(userScore, computerScore);
 }
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    let clickChoice = button.id;
+    playGame(clickChoice);
+  });
+});
